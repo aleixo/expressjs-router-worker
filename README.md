5a940e342801ac0b6c83e00ab7a58a11536bba1a
Diogo Aleixo <daleixo@gocontact.pt>
2018-04-22 23:00:19 +0100

    [ADD] gitignore, worker, npm init

---
+++ README.md
@@ -0,0 +1,14 @@
+# expressjs-router-worker
+
+This module helps newcomers to build one router event if they do not understand the routing.
+
+In fact. It is for my usage, but anyone is welcome to use and/or sugest improvements.
+
+## Example
+
+There is one usage example in ```example_usage.js``` and ```app.js``` files.
+
+## TODO
+
+1. Provide other registering mechanisms or allow to inject a way you want output response.
+2. Throw errors in case router is not well configured
---
+++ RouterWorker.js
@@ -0,0 +1,67 @@
+const { Router } = require('express');
+
+/**
+ * This class allows other routers to have one worker.
+ * Every router must implement this class
+ *
+ * @class RouterWorker
+ */
+module.exports = class RouterWorker {
+    /**
+     * Creates an instance of RouterWorker.
+     *
+     * @memberof RouterWorker
+     */
+    constructor () {
+        this.router = Router();
+    }
+
+    /**
+     * Returns the express router instance of the class.
+     *
+     * @returns {Object} The configured router.
+     * @memberof RouterWorker
+     */
+    get configuredRouter () {
+        return this.router;
+    }
+
+    /**
+     * Sets the router controller to be user when registering the route.
+     *
+     * @param {Object} controller - The controller responsible for the flow.
+     * @memberof RouterWorker
+     */
+    set controller (controller) {
+        this.workerController = controller;
+    }
+
+    /**
+     * Setter to set the router entry point.
+     *
+     * @param {string} entry - One string representing the entry router path.
+     * @memberof RouterWorker
+     */
+    set entry (entry) {
+        this.routerEntry = entry;
+    }
+    
+    /**
+     * Register one new route on the router and setts ist callback.
+     *
+     * @param {Object} config - The object with the configurations for the endpoint.
+     * @memberof RouterWorker
+     */
+    registerRoute (config) {
+        console.log(`[REGISTER ROUTE] ${config.method} -> ${this.routerEntry}${config.path}`);
+        this.router[config.method](config.path, (req, res, next) => {
+            let mergedRequest = Object.assign(req.params, req.body);
+            mergedRequest = Object.assign(mergedRequest, req.query);
+            mergedRequest = Object.assign(mergedRequest, req.files);
+            
+            this.workerController[config.controllerAction](mergedRequest)
+                .then(result => res.json(result))
+                .catch(next);
+        });            
+    }
+};
